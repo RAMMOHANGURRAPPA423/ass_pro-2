0e56ad81b6945e939696754581097bb766a7837d
gurrappa <gurrappa.p@thundersoft.com>
2022-02-18 21:30:30 +0530

    committing assignments

---
+++ p2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int *p=arr;
+    for(int i=0;i<n;i++)
+    printf("%d\n",*p+i);
+}
---
+++ p4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define PI 3.14
+int main(){
+ enum formula{area, square, rectangle};
+   // float area;
+    //int square;
+    //int rectangle;
+    int l=5,b=7,s=4;
+    int r=5,choice;
+    printf("enter choice");
+    scanf("%d",&choice);
+    switch(choice){
+
+    case 1:
+    area=PI*r*r;
+    printf("area of the circle is %f",area);
+    break;
+
+case 2:
+square=s*s;
+printf("area of square %d",square);
+break;
+case 3:
+ rectangle=l*b;
+printf("area of rectanglr %d",rectangle);
+break;
+default:
+printf("print nothing");
+}
+
